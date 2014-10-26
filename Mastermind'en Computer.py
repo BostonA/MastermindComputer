639f45411cfed97d89da3dffd2f5dff17e987f67
BostonA <BostonWAbrams@gmail.com>
2014-10-26 15:26:50 -0700

    Fuctionalize and add comment

--- Mastermind'en Computer.py
+++ Mastermind'en Computer.py
@@ -1,2 +1,7 @@
 import random
-print random.randint(1, 6)
+def Choose_1_random_color():
+    #Choses random color (For First turn)
+    print random.randint(1, 6)
+
+#BODY BELOW
+Choose_1_random_color()
