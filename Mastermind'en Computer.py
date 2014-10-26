328cb52750e89739b9a9beb1403fc00e21ee7411
BostonA <BostonWAbrams@gmail.com>
2014-10-26 15:36:48 -0700

    Switch to color insted of int()

--- Mastermind'en Computer.py
+++ Mastermind'en Computer.py
@@ -1,7 +1,29 @@
 import random
 def Choose_1_random_color():
     #Choses random color (For First turn)
-    print random.randint(1, 6)
+    TheColorNum = random.randint(1, 6)
+    #TheColorNum is the number before it is switched to a string and a color
+    #(Blue, Green...)
+    if (TheColorNum == 1):
+        TheColor = "Blue"
+    elif (TheColorNum == 2):
+        TheColor = "Green"
+    elif (TheColorNum == 3):
+        TheColor = "Yellow"
+    elif (TheColorNum == 4):
+        TheColor = "White"
+    elif (TheColorNum == 5):
+        TheColor = "Orange"
+    elif (TheColorNum == 6):
+        TheColor = "Green"
+    else:
+        #Bug Check
+        TheColor = "BUG HERE STOP"
+    #That made the .randit into a string (color)
+    #TheColor = The color is chosen
+    return TheColor
+    
+    
 
 #BODY BELOW
-Choose_1_random_color()
+print Choose_1_random_color()
