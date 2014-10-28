0de1c2727dfe735f92089f7957e3634b8942a06c
BostonA <BostonWAbrams@gmail.com>
2014-10-27 18:40:57 -0700

    More Fuctions
    - UserPlay()
    - HowManyPegs()
    - MakeUserStatFile() #Not Using
    - Edited Email
    - Edited Terms of Sevace
    - Added main code stuff

--- Mastermind'en Computer (MasterMind).py
+++ Mastermind'en Computer (MasterMind).py
@@ -25,25 +25,36 @@ def Choose_1_random_color():
     return TheColor
 def InteractionAtBeganing(TitleText):
     TermsOfSevace = "Terms of Sevace \n \n  By Using this program you agree to not report an invalid bug, and to allow Boston Abrams and others,to use the data that you perduse, your email  adress and your name. "
-    
-    easygui.msgbox(msg = TermsOfSevace, title = TitleText +  " Terms of sevace", ok_button = "I agree")            
+    easygui.msgbox(msg = TermsOfSevace, title = TitleText +  " Terms of sevace", ok_button = "I Agree")            
     easygui.msgbox(msg = "I have chosen a code for you to break", title = TitleText +  " Intoduction", ok_button = "Next")
     easygui.msgbox(msg = "There is no way you can guess it", title = TitleText +  " Intoduction", ok_button = "Next")
-
     easygui.msgbox(msg = "But I will give you a try", title = TitleText +  " Intoduction", ok_button = "Next")
     Name = easygui.enterbox(msg = "Enter Your nic-name (what you want to be called by in the game)", title = TitleText)
-    easygui.enterbox(msg = "Enter your email address", title = Name + " " + TitleText + " Email Address.")
+    Email = easygui.enterbox(msg = "Enter your email address", title = Name + " " + TitleText + " Email Address.")
 
-def UserPlay():
-    easygui.buttonbox(msg = "Pick the color for the color of your leftmost hole", title = "Play Mastermind (By Boston Abrams)Copyright 2014 Boston Abrams", choices = ("Blue", "Green", "White", "Red", "Yellow", "Orange"))
-    
+def UserPlay(TitleText):
+    #Propts the user enter their guess
+    ColorChosen = []
+    easygui.msgbox(msg = "Enter Your Guesses in the boxes that follow ", title = TitleText +  " Guessing", ok_button = "Next")
+    ColorChosen[0] = easygui.buttonbox(msg = "Pick the color for your 1st hole", title = TitleText + " Guessing", choices = ("Blue", "Green", "White", "Red", "Yellow", "Orange"))
+    ColorChosen[1] = easygui.buttonbox(msg = "Pick the color for your 2nd hole", title = TitleText + " Guessing", choices = ("Blue", "Green", "White", "Red", "Yellow", "Orange"))
+    ColorChosen[3] = easygui.buttonbox(msg = "Pick the color for your 3nd hole", title = TitleText + " Guessing", choices = ("Blue", "Green", "White", "Red", "Yellow", "Orange"))
+    ColorChosen[4] = easygui.buttonbox(msg = "Pick the color for your 4nd hole", title = TitleText + " Guessing", choices = ("Blue", "Green", "White", "Red", "Yellow", "Orange"))
+    return ColorChosen
+def HowManyPegs(ColorsOfUsersGuess, MasterCode):
+    Counter = 0 # counts number of ColorsOfUsersGuess's to match the Master Code
+    #Figures out how many pegs
+    Red = 0 #How many reds
+    White = 0 #how many Whites
+    while counter > 5:
+        if ColorsOfUsersGuess[0] == MasterCode[counter]:
+            pass
+def MakeUserStatFile(PlayerEmail, PlayerName, WhoWon):
+    things = open(PlayerName + " Mastermind Stat doc.txt", "a")
+    things.write("During Game 1 " + WhoWon + " Won")
+    things.close()
 
-def AskUserWhatTheyWantToPlay():
-    #Red and White are the color of the small pegs
-    Red = raw_input("How many red Pegs: ")
-    White = raw_input("How many White Pegs: ")
-    return Red, White
 Code = [Choose_1_random_color(), Choose_1_random_color(),Choose_1_random_color(),Choose_1_random_color()]
 InteractionAtBeganing(TitleText)
-UserPlay()
-
+Colors = UserPlay()
+HowManyRedPegs(Colors, Code)
